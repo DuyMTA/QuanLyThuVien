cc6da02a58b9fd5990233f37c9bdffaf9284cf7e
HoVan98 <71630082+HoVan98@users.noreply.github.com>
2020-12-18 09:08:16 +0700

    Revert "Revert "Revert "Revert "Revert "Merge pull request #1 from phamxuantien27/master"""""
    This reverts commit dff02846fd013ce43f6e997ccd7e4c2ca5aee956.

--- QuanLyThuVien/BUS/Properties/AssemblyInfo.cs
+++ QuanLyThuVien/BUS/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("89f2dfdb-da8a-4d25-905d-da0b83b658b8")]
+[assembly: Guid("51ed7520-66d8-45f9-b6ed-7fadff6b5908")]
 
 // Version information for an assembly consists of the following four values:
 //
